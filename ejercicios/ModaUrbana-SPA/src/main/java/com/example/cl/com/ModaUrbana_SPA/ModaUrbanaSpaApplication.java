b8606815e861520fa9b5f4a6dbc638a8cf96ee2b
R0666 <r.jerez@duocuc.cl>
2025-05-23 16:55:43 +0100

    Subiendo ejercicio hospital

---
+++ ejercicios/ModaUrbana-SPA/src/main/java/com/example/cl/com/ModaUrbana_SPA/ModaUrbanaSpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.cl.com.ModaUrbana_SPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModaUrbanaSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModaUrbanaSpaApplication.class, args);
+	}
+
+}
---
+++ ejercicios/ModaUrbana-SPA/src/test/java/com/example/cl/com/ModaUrbana_SPA/ModaUrbanaSpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cl.com.ModaUrbana_SPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModaUrbanaSpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
