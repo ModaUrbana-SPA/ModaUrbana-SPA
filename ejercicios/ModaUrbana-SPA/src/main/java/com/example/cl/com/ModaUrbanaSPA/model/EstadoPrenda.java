6a5913068a021174e9c90b526c5fccdba95f174b
r.jerez <r.jerez@duocuc.cl>
2025-06-13 15:21:03 +0100

    Actualizado EstadoPRENDA SOLO CLASE

--- ejercicios/ModaUrbana-SPA/src/main/java/com/example/cl/com/ModaUrbanaSPA/model/EstadoPrenda.java
+++ ejercicios/ModaUrbana-SPA/src/main/java/com/example/cl/com/ModaUrbanaSPA/model/EstadoPrenda.java
@@ -1,5 +1,23 @@
 package com.example.cl.com.ModaUrbanaSPA.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ESTADO_PRENDA")
 public class EstadoPrenda {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id_estado_prenda;
+
+    @Column(nullable = false)
+    private LocalDateTime hora_llegada;
+
+    @Column(length = 50, nullable = false)
+    private String estado;
 }
--- ejercicios/ModaUrbana-SPA/src/main/java/com/example/cl/com/ModaUrbanaSPA/model/Prenda.java
+++ ejercicios/ModaUrbana-SPA/src/main/java/com/example/cl/com/ModaUrbanaSPA/model/Prenda.java
@@ -3,16 +3,13 @@ package com.example.cl.com.ModaUrbanaSPA.model;
 import jakarta.persistence.*;
 import lombok.*;
 
-
-
-// lombok
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-// DATA
 @Entity
 @Table(name = "PRENDA")
 public class Prenda {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id_prenda;
@@ -29,13 +26,14 @@ public class Prenda {
     @Column(nullable = false)
     private String color;
 
-    @Column(nullable = false)
-    private Integer stock;
-
     @Column(length = 100, nullable = false)
     private String imagen;
 
     @ManyToOne
     @JoinColumn(name = "id_tipo_prenda", referencedColumnName = "id_tipo_prenda")
     private TipoPrenda id_tipo_Prenda;
+
+    @ManyToOne
+    @JoinColumn(name = "id_estado_prenda", referencedColumnName = "id_estado_prenda")
+    private EstadoPrenda estadoPrenda;
 }
