393ab3fcabd7adb505a0a480bfbf91c325793293
moi.rodriguez <moi.rodriguez@duocuc.cl>
2025-06-13 00:26:08 -0400

    Creé controlador, servicio y repositorio de TipoPrenda

---
+++ ejercicios/ModaUrbana-SPA/src/main/java/com/example/cl/com/ModaUrbanaSPA/repository/TipoPrendaRepositorio.java
@@ -0,0 +1,10 @@
+package com.example.cl.com.ModaUrbanaSPA.repository;
+
+
+import com.example.cl.com.ModaUrbanaSPA.model.TipoPrenda;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TipoPrendaRepositorio extends JpaRepository<TipoPrenda, Long> {
+}
